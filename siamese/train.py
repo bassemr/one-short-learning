49d6a5d1fcb8b77c2fe6168f8c5d056c27a47ebd
Bassem <roccokedis@gmail.com>
2025-09-30 19:23:18 +0200

    update

--- siamese/train.py
+++ siamese/train.py
@@ -7,6 +7,7 @@ from torchvision import transforms
 from torch.utils.tensorboard import SummaryWriter
 from tqdm.auto import tqdm
 from typing import Dict, List, Tuple
+from pathlib import Path
 
 
 # Set seeds
@@ -167,5 +168,37 @@ def train(model, train_loader, valid_loader, optimizer, loss_fn, device,
         writer.close()
 
     return history, model
+
+
+
+def save_model(model: torch.nn.Module,
+               target_dir: str,
+               model_name: str):
+  """Saves a PyTorch model to a target directory.
+
+  Args:
+    model: A target PyTorch model to save.
+    target_dir: A directory for saving the model to.
+    model_name: A filename for the saved model. Should include
+      either ".pth" or ".pt" as the file extension.
+
+  Example usage:
+    save_model(model=model_0,
+               target_dir="models",
+               model_name="05_going_modular_tingvgg_model.pth")
+  """
+  # Create target directory
+  target_dir_path = Path(target_dir)
+  target_dir_path.mkdir(parents=True,
+                        exist_ok=True)
+
+  # Create model save path
+  assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
+  model_save_path = target_dir_path / model_name
+
+  # Save the model state_dict()
+  print(f"[INFO] Saving model to: {model_save_path}")
+  torch.save(obj=model.state_dict(),
+             f=model_save_path)
 
 
